9abaf59cf2e3d2de59857e100ce6dcad2f81375a
boazjames <boazjames2@yahoo.com>
2025-01-25 10:07:41 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "JBTextfield",
     defaultLocalization: "en",
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        // Products define the executables and libraries a package produces, and make them visible to other packages. 
         .library(
             name: "JBTextfield",
             targets: ["JBTextfield"]),
